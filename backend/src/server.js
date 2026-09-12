28b81f10d713893ddd835a1beafc5ef58ce33594
phenomgithub <sharifshaghil1999@gmail.com>
2026-09-12 13:02:52 +0530

    chore:simplify cors to universal origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 5000;
 //Middleware configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://vercel.app"],
-    credentials: true,
+    origin: "*",
   }),
 );
 app.use(express.json());
